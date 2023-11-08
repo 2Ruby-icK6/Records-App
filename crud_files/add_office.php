7330264afe6d0a58ea9f2aafdb09540674ab4357
Delcoro, Jofli Asaph <asaphdelcoro987@gmail.com>
2023-11-08 18:07:43 +0800

    added input html form to add_office.php

--- crud_files/add_office.php
+++ crud_files/add_office.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Add Office</title>
+</head>
+<body>
+    <h1>ADD OFFICE</h1>
+
+    <form action="" method="POST">
+        <label>Office Name:</label><input type="text" name="office_name"><br></br>
+        <label>Contact Number:</label><input type="text" name="contact_num"><br></br>
+        <label>Email:</label><input type="text" name="email"><br></br>
+        <label>Address:</label><input type="text" name="address"><br></br>
+        <label>city:</label><input type="text" name="city"><br></br>
+        <label>country:</label><input type="text" name="country"><br></br>
+        <label>postal:</label><input type="text" name="postal"><br></br>
+
+        <button type="submit" name="submit">Submit</button>
+</body>
+</html>
 <?php
-require('**/config/config.php');
-require('**/config/db.php');    
+require('..\config\config.php');
+require('..\config\db.php');    
