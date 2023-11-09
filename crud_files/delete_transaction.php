905741efac3299b08474d1d4cb096b53ec464200
2Ruby-icK6 <202180232@psu.palawan.edu.ph>
2023-11-09 08:38:31 +0800

    Added Code on  Delete File

--- crud_files/delete_transaction.php
+++ crud_files/delete_transaction.php
@@ -1,3 +1,24 @@
 <?php
-require('**/config/config.php');
-require('**/config/db.php');
+    require('../config/config.php'); 
+    require('../config/db.php');
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+    
+
+        $query = 'DELETE FROM transaction WHERE id='. $id;
+
+        //Get the Result
+        $result =  mysqli_query($conn, $query);
+
+        //Free Result
+        mysqli_free_result($result);
+
+        //Close the Connection
+        mysqli_close($conn);
+    }
+
+header("location: /Records-App/transaction.php");
+exit;
+
+?>
